fbc6a92f7b8d72c49ffe28fbf172465251099cf5
dlehgml0901 <dlehgml0901@example.com>
2020-05-18 18:17:35 +0900

    2020-05-18 18:17

--- src/frontend/src/components/Home.vue
+++ src/frontend/src/components/Home.vue
@@ -10,7 +10,8 @@
                 {{self.menu}}
                 <ul class="menu">
                     <li v-for="i of sidebars" :key="i.menu">
-                        {{i.menu}}
+                        <!--<a @click="menu(i.menu)">{{i.menu}}</a>-->
+                        <router-link :to="{path: i.link}">{{i.menu}}</router-link>
                     </li>
                 </ul>
             </template>
@@ -31,12 +32,37 @@
         data: ()=> {
             return {
                 sidebars: [
-                    {menu: "쓰기"},
-                    {menu: "목록"},
-                    {menu: "검색"},
-                    {menu: "수정"},
-                    {menu: "삭제"}
+                    {menu: '등록', link:'/register'},
+                    {menu: '목록', link:'/list'},
+                    {menu: '검색', link:'/search'},
+                    {menu: '수정', link:'/update'},
+                    {menu: '삭제', link:'/delete'},
+                    {menu: '회원수', link:'/vuexCounter'}
                 ]
+            }
+        },
+        methods:{
+            menu(i){
+                switch (i) {
+                    case '쓰기':
+                        alert('0')
+                        break;
+                    case '목록':
+                        alert('1')
+                        break;
+                    case '검색':
+                        alert('2')
+                        break;
+                    case '수정':
+                        alert('3')
+                        break;
+                    case '삭제':
+                        alert('4')
+                        break;
+                    case '회원수':
+                        alert('5')
+                        break;
+                }
             }
         }
     }
---
+++ src/frontend/src/components/common/Counter.vue
@@ -0,0 +1,30 @@
+<template>
+    <div>
+        <h1>카운터</h1>
+        <div>{{ count }}</div>
+        <div @click="counter" class="btn operator">클릭수 올리기</div>
+        <div @click="reset" class="btn operator">초기화</div>
+    </div>
+</template>
+
+<script>
+    export default {
+        data () {
+            return {
+                count: 0
+            }
+        },
+        methods: {
+            counter() {
+                this.count++
+            },
+            reset(){
+                this.count = 0
+            }
+        }
+    }
+</script>
+
+<style scoped>
+
+</style>
